50986510b5b8b5d617a524480e4c7364ce862365
SAID444-hash <saidhassano658@gmail.com>
2026-06-22 14:45:16 +0300

    Configure CORS, install Axios, create API client with interceptors and Vite proxy

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -55,6 +55,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 });
-
-
-
